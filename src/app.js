6c489c84129217298cfc0b672bd8bb62319f3f20
RNogales94 <rnogales@correo.ugr.es>
2018-04-30 17:32:48 +0200

    katka

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ var express  = require("express"),
     methodOverride = require('method-override'),
     bodyParser = require('body-parser');
 
+const uri_mongo = 'mongodb+srv://isi-read-only-user:OYKrQGLp5582EtSu@cluster0-fjo6d.mongodb.net/test'
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
@@ -14,12 +16,16 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 
 router.get('/', function(req, res) {
-   res.send("Hello World!");
+   res.send("Ahoj světe!");
+});
+
+router.get('/katka', function(req, res) {
+   res.send("Ahoj Katka!");
 });
 
 app.use(router);
 
-mongoose.connect('mongodb+srv://isi-read-only-user:OYKrQGLp5582EtSu@cluster0-fjo6d.mongodb.net/test', function(err, res) {
+mongoose.connect(uri_mongo, function(err, res) {
   if(err) {
     console.log('ERROR: connecting to Database. ' + err);
   }
