ae18237f4393b667ca68004ed4c1af6d295b4bad
RNogales <rnogales@correo.ugr.es>
2018-04-30 14:15:36 +0200

    Test mongo on cloud

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var MongoClient = require('mongodb').MongoClient;
 
+var uri = "mongodb+srv://isi-read-only-user:OYKrQGLp5582EtSu@cluster0-fjo6d.mongodb.net/test";
+
 MongoClient.connect(uri, function(err, client) {
    const collection = client.db("test").collection("devices");
    // perform actions on the collection object
