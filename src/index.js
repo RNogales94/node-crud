9a549a6f79ff615e977ab1042209c2c6be951f7a
RNogales <rnogales@correo.ugr.es>
2018-04-30 13:01:01 +0200

    Seleccionar puerto dinamicamente (Heroku)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var port = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
 
@@ -9,7 +10,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(port);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at port ' + port);
